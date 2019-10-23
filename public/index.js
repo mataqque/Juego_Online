7572628e2e2c0f7eed89ea8c6076089aca8beded
Flavio <mataqque.100@gmail.com>
2019-10-22 19:56:43 -0500

    generando codigo

--- public/index.js
+++ public/index.js
@@ -4,18 +4,24 @@ const http = require("http").Server(app);
 const socket = require("socket.io")(http)
 const url = process.env.PORT || 3000;
 
+var usuarios = []
+
 http.listen(3000,function(){
     console.log("Server iniciado:",3000)
 });
 
 app.use(express.static(__dirname+"/public"));
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/public/index.html");
-})
+});
 
 socket.on("connection",function(io){
     io.on("mensaje",function(msg){
+        usuarios.push(msg)
         console.log(msg)
         socket.emit("mensaje",msg)
-    })
-})
+    });
+});
+
+ 
--- public/public/connecion.js
+++ public/public/connecion.js
@@ -1,15 +1,12 @@
 var socket = io();
-// console.log(socket)
 
-socket.emit("mensaje","hola");
-socket.on("mensaje",function(msg){
-    console.log(msg)
-})
-
+var usuario;
+var usuarios = document.getElementsByClassName("datos")[0];
+var configuracion = {id:""};
 // movimiento del cuadrado 
 var movimiento = "";
 var arriba = 0;abajo = 0;derecha = 0;izquierda = 0;
-var num = 10;
+var num = 4;
 var cuadrado = document.getElementsByClassName("cuadrado")[0];
 var alimento_x = 0
 var alimento_y = 0
@@ -37,16 +34,13 @@ function cordenada_izquierda(){ izquierda = izquierda - num ; derecha = izquierd
 function cordenada_derecha(){ derecha = derecha + num ; izquierda = derecha; return derecha;}
 
 function colision(posicion_top){
-    let arriba_colision = alimento_y -14;
+    let arriba_colision = alimento_y - cuadrado.getBoundingClientRect().height;
     let arriba_colision1 =  alimento_y + 14;
-    let left_colision = alimento_x - 14;
-    let left_colision1 = alimento_x + 14;
-
-    console.log(derecha)
-    console.log(left_colision) 
+    let left_colision = alimento_x - cuadrado.getBoundingClientRect().width;
+    let left_colision1 = alimento_x + 14; 
     
     if( derecha > left_colision && derecha < left_colision1 && arriba > arriba_colision && arriba < arriba_colision1 ){
-        let width1 =cuadrado.getBoundingClientRect().width;
+        let width1 = cuadrado.getBoundingClientRect().width;
         let height1 =cuadrado.getBoundingClientRect().height;
         cuadrado.style.width = width1+3+"px";
         cuadrado.style.height = height1+3+"px"
@@ -60,14 +54,43 @@ function colision(posicion_top){
 
 var  alimento = document.getElementsByClassName("alimento")[0];
 function posicion_azar(){
-    let x = (document.getElementsByClassName("game")[0].getBoundingClientRect().width)-20;
-    let y = (document.getElementsByClassName("game")[0].getBoundingClientRect().height)-35;
-    alimento_x = Math.random()*x;
-    alimento_y = Math.random()*y
-    console.log(`width: ${x} \n height:${y}`)
-    // alimento.style.top = 50+"px";
-    // alimento.style.left = 20+"px";
+    let x = (document.getElementsByClassName("game")[0].getBoundingClientRect().width)-20; //limite para pasar
+    let y = (document.getElementsByClassName("game")[0].getBoundingClientRect().height)-35; //limete para pasar
+    alimento_x = Math.floor(Math.random()*x);
+    alimento_y = Math.floor(Math.random()*y);
+
     alimento.style.top = (alimento_y)+"px";
     alimento.style.left = (alimento_x)+"px";
 }
 posicion_azar();
+
+// connecion usuario 
+
+document.getElementById("enviar").addEventListener("click",function(event){
+    usuario = document.getElementById("id_usuario").value;
+    document.getElementById("id_usuario1").textContent = usuario+` : `;
+    event.preventDefault();
+    configuracion.id = usuario;
+    llamada_socket();
+});
+
+socket.on("mensaje",function(msg){
+    nuevo_participante(msg);
+});
+
+function llamada_socket(){
+    socket.emit("mensaje",configuracion);
+    console.log(configuracion)
+    socket.on("mensaje",function(msg){
+        console.log(msg)
+        nuevo_participante(msg);
+    });
+}
+function nuevo_participante(datos){
+    console.log(datos)
+    for(let i = 0;i<1;i++){
+        usuarios.innerHTML = `<div>
+        <span id="id_usuario1">${datos.id} : </span><span id="puntaje1">1000</span>
+    </div>`
+    }
+}
